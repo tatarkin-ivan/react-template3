1d76a7d7f1d1cd727753527e169493bc80245c2f
tatarkin <mobtaxi@gmail.com>
2022-11-04 17:55:28 +0300

    change home

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import s from './Home.module.css';
+
+function Home(){
+    return(
+        <div className={s.AllComponent}>
+            123456
+        </div>
+    );
+};
+
+export default Home;
